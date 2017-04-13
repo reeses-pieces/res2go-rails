6c96857b309d96c7b640ae102763a08191e142fd
reeses-pieces <nicholasreis@icloud.com>
2017-04-13 16:55:18 -0700

    add new stat route

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,24 @@
 class StatsController < ApplicationController
 
+  def new
+    @user = current_user
+    @stat = Stat.new
+    @reservoirs = Reservoir.all
+  end
+
+  def create
+    @reservoir = Reservoir.find_by(name: params["reservoir_name"])
+    @stat = Stat.new(user_id: current_user.id,
+                     reservoir_id: @reservoir.id,
+                     min: params["min"],
+                     max: params["max"])
+    if @stat.save
+      redirect_to user_path(current_user)
+    else
+      redirect_to new_stat_path(@stat)
+    end
+  end
+
   def edit
     @stat = Stat.find(params[:id])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 end
