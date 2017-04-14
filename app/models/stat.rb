911c4d0cbd44449016652f2c04bececec7a59217
reeses-pieces <nicholasreis@icloud.com>
2017-04-14 11:34:38 -0700

    add correct stat validations

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -3,9 +3,8 @@ class Stat < ApplicationRecord
   belongs_to :user
   belongs_to :reservoir
 
-  # validates :reservoir, presence: true
-  # validates_numericality_of :min, :greater_than => 0, :less_than => 100, :message => "Must be between 1 and 99"
-  # validates_numericality_of :max, :greater_than => 0, :less_than => 100, :message => "Must be between 1 and 99"
+  validates :reservoir_id, presence: true
+  validates_numericality_of :min, :greater_than => 0, :less_than => 100, :message => "Must be between 1 and 99", allow_blank: true
+  validates_numericality_of :max, :greater_than => 0, :less_than => 100, :message => "Must be between 1 and 99", allow_blank: true
 
-  # validates :min, :max, numericality: true
 end
