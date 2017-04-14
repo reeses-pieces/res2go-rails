5b5dbb5ac9faacea9f267ca83fa7749cea8bb3ba
reeses-pieces <nicholasreis@icloud.com>
2017-04-14 13:00:16 -0700

    add config line to serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #CUSTOM
+  config.serve_static_assets = true
 end
