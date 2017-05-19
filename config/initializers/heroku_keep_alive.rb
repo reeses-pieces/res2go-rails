558faea49e0a4f190c8ab4dee5c3203f6085173b
Nick Reis <nicholasreis@gmail.com>
2017-05-19 14:16:54 -0700

    modify heroku pinger schedule

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -2,7 +2,7 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
 if Rails.env.production?
-  scheduler.every '10m' do
+  scheduler.cron '0 6-19 * * *' do
      require "net/http"
      require "uri"
      Net::HTTP.get_response(URI.parse(ENV["HOSTNAME"]))
