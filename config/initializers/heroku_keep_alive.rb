18a794ffd5eef42b5eaa4da8384a60fa0b39442d
reeses-pieces <nicholasreis@icloud.com>
2017-05-21 12:56:38 -0700

    change server time to PST

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -2,7 +2,7 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
 if Rails.env.production?
-  scheduler.cron '0 6-19 * * *' do
+  scheduler.cron '0 9-21 * * *' do
      require "net/http"
      require "uri"
      Net::HTTP.get_response(URI.parse(ENV["HOSTNAME"]))
